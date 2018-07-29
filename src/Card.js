0c03bb38835d80fa6ad01e13bd03309537ef88a4
Hamidreza <htondkar@gmail.com>
2018-07-29 21:15:35 +0430

    experiment with queue-animation

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Flipped } from 'react-flip-toolkit'
 import Fade from './Fade'
+// import QueueAnim from 'rc-queue-anim';
+
 
 const Card = ({ title, image, position }) => (
   <Flipped flipId="card" key="1">
     <article className="card" data-position={position}>
       <Fade>
-        <img className="card__image" src={image} alt="cat" />
-        <header className="card__title">{title}</header>
+      {/* <QueueAnim type="alpha"> */}
+        <img key="1" className="card__image" src={image} alt="cat" />
+        <header key="2" className="card__title">{title}</header>
+      {/* </QueueAnim> */}
       </Fade>
     </article>
   </Flipped>
